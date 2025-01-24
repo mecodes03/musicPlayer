98531bcd9bee00a38f9b0642ee3ee8ac2e7cec1f
harsh <harshvaish2003@gmail.com>
2025-01-24 05:59:32 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
+    output: 'standalone',
+    images: {
     remotePatterns: [
       {
         protocol: "https",
@@ -20,8 +21,21 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
-    ],
-  },
+        ],
+    },
+    env: {
+        POSTGRES_DATABASE_CONNECTION_STRING: process.env.DATABASE_URL,
+	    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+        DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
+        YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URL,
+        SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+        SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL,
+        BASE_URL: process.env.BASE_URL,
+		UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_BASE_URL,
+    }
 };
-
 export default nextConfig;
